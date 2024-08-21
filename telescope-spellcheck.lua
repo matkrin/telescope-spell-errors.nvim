4ab76ecaa9885f31033b8b74b08a239aacce6cab
matkrin <matkrin@protonmail.com>
2024-08-21 22:11:45 +0200

    Spellcheck picker

--- telescope-spellcheck.lua
+++ telescope-spellcheck.lua
@@ -2,7 +2,6 @@ local pickers = require "telescope.pickers"
 local finders = require "telescope.finders"
 local conf = require("telescope.config").values
 
--- (`{[1]: string, [2]: 'bad'|'rare'|'local'|'caps', [3]: integer}[]`)
 
 local function get_spell_errors()
     local filename = vim.api.nvim_buf_get_name(0)
@@ -25,13 +24,32 @@ local function get_spell_errors()
     return checked
 end
 
--- print(vim.inspect(get_spell_errors()))
--- print("OK")
-local spell_bad = vim.api.nvim_get_hl(0, { name = "SpellBad" })
-print(vim.inspect(spell_bad))
-vim.api.nvim_set_hl(0, "TelescopeSpellCheckBad", { fg = spell_bad.sp })
+-- Highlight group names
+local BAD = "TelescopeSpellCheckBad"
+local RARE = "TelescopeSpellCheckRare"
+local LOCAL = "TelescopeSpellCheckLocal"
+local CAPS = "TelescopeSpellCheckCap"
 
--- our picker function: colors
+local hl_map = {
+    _bad = BAD,
+    _rare = RARE,
+    _local = LOCAL,
+    _caps = CAPS,
+}
+
+-- Get highlight groups
+local spell_bad = vim.api.nvim_get_hl(0, { name = "SpellBad" })
+local spell_cap = vim.api.nvim_get_hl(0, { name = "SpellCap" })
+local spell_rare = vim.api.nvim_get_hl(0, { name = "SpellRare" })
+local spell_local = vim.api.nvim_get_hl(0, { name = "SpellLocal" })
+
+-- Create hightlight groups (tokyonight uses undercurls, `sp` is the color)
+vim.api.nvim_set_hl(0, BAD, { fg = spell_bad.sp })
+vim.api.nvim_set_hl(0, CAPS, { fg = spell_cap.sp })
+vim.api.nvim_set_hl(0, RARE, { fg = spell_rare.sp })
+vim.api.nvim_set_hl(0, LOCAL, { fg = spell_local.sp })
+
+
 local spellcheck = function(opts)
     opts = opts or {}
     pickers.new(opts, {
@@ -39,23 +57,21 @@ local spellcheck = function(opts)
         finder = finders.new_table {
             results = get_spell_errors(),
             entry_maker = function(entry)
-                local pos = string.format("%4d:%2d", entry.line_num, entry.col_num)
-                -- local spell_bad = vim.api.nvim_get_hl(0, {name = "SpellBad"})
-                -- local spell_cap = vim.api.nvim_get_hl(0, {name = "SpellCap"})
-                -- local spell_rare = vim.api.nvim_get_hl(0, {name = "SpellRare"})
-                -- local spell_local = vim.api.nvim_get_hl(0, {name = "SpellLocal"})
-                -- local spell_bad = vim.api.nvim_get_hl(0, { name = "SpellBad" })
-                -- print(vim.inspect(spell_bad))
-                -- vim.api.nvim_set_hl(0, "TelescopeSpellCheckBad", { fg = spell_bad.sp })
+                local pos = string.format("%4d:%3d", entry.line_num, entry.col_num)
+                local type_and_pos = string.format("%-5s", string.upper(entry.error_type)) .. pos
+                local highlight_len = string.len(type_and_pos)
                 return {
                     value = entry,
-                    display = string.upper(entry.error_type) .. " " .. pos .. " ▏" .. entry.word,
-                    ordinal = entry.word,
+                    display = function()
+                        return type_and_pos .. "  ▏ " .. entry.word,
+                            { { { 0, highlight_len }, hl_map["_" .. entry.error_type] } }
+                    end,
+                    ordinal = type_and_pos,
                     filename = entry.filename,
                     type = entry.error_type,
                     lnum = entry.line_num,
                     col = entry.col_num,
-                }, { { 1, 4 }, "DiagnosticError" }
+                }
             end
         },
         previewer = conf.qflist_previewer(opts),
@@ -66,5 +82,4 @@ local spellcheck = function(opts)
     }):find()
 end
 
--- to execute the function
 spellcheck()
