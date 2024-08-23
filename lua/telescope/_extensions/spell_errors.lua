ef92fd1a83ec87b0993faf15b13906bbae08e955
matkrin <matkrin@protonmail.com>
2024-08-23 13:02:25 +0200

    Rename to spell_errors.lua

--- lua/telescope/_extensions/spell_errors.lua
+++ lua/telescope/_extensions/spell_errors.lua
@@ -1,43 +1,54 @@
-local pickers = require "telescope.pickers"
-local finders = require "telescope.finders"
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
 local conf = require("telescope.config").values
 
 
---- return {row, col}, row 1-based, col 0-based
+-- All with `_` as local is also a keyword
+local hl_groups = {
+    _bad = "TelescopeSpellErrorBad",
+    _rare = "TelescopeSpellErrorRare",
+    _local = "TelescopeSpellErrorLocal",
+    _caps = "TelescopeSpellErrorCap",
+}
+
+
+--- Get the cursor position in the buffer
+---@return integer[] { row, col } The current cursor position (row: 1-based, col: 0-based)
 local function get_cursor_pos()
     return vim.api.nvim_win_get_cursor(0)
 end
 
---- pos: {row, col}, row 1-based, col 0-based
+--- Set the cursor position in the buffer
+---@param pos integer[] {row, col} (row 1-based, col 0-based)
 local function set_cursor_position(pos)
     return vim.api.nvim_win_set_cursor(0, pos)
 end
 
-local function get_word_under_cursor()
-    return vim.fn.expand("<cword>")
-end
-
+--- Jump to the first spell error
 local function go_to_first_spell_error()
-    -- vim.api.nvim_feedkeys("gg0]s", "n", true)
-    -- vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("gg0]s", true, false, true), 'n', true)
-    -- vim.api.nvim_input("gg0]s")
+    -- Needs to be a synchronous call, so not `vim.api.nvim_feedkeys` or
+    --  `vim.api.nvim_input` not possible,
+    --  otherwise it ends up in the telescope prompt
     vim.api.nvim_command("silent normal! gg0]s")
 end
 
+--- Jump to the next spell error
 local function go_to_next_spell_error()
-    -- vim.api.nvim_feedkeys("]s", "n", true)
-    -- vim.api.nvim_input("]s")
     vim.api.nvim_command("silent normal! ]s")
 end
 
+
+-- Tried the approach of just checking each line with `vim.spell.check`,
+--  but gave a lot more results that are not showing up in the buffer.
+--  Also context would be missing
+
+---@return table[] spellerrors
 local function get_spell_errors()
     local filename = vim.api.nvim_buf_get_name(0)
     local original_pos = get_cursor_pos()
 
-    -- Move to the first spell error
     go_to_first_spell_error()
 
-    -- Initialize spell errors table
     local spellerrors = {}
 
     local first_pos = get_cursor_pos()
@@ -64,55 +75,7 @@ local function get_spell_errors()
 end
 
 
--- local function get_spell_errors()
---     local filename = vim.api.nvim_buf_get_name(0)
---     local lines = vim.api.nvim_buf_get_lines(0, 0, vim.api.nvim_buf_line_count(0), false)
---     local checked = {}
---     for i, line in ipairs(lines) do
---         local line_checked = vim.spell.check(line)
---         if not vim.tbl_isempty(line_checked) then
---             for _, spell_err in ipairs(line_checked) do
---                 table.insert(checked, {
---                     filename = filename,
---                     word = spell_err[1],
---                     error_type = spell_err[2],
---                     line_num = i,
---                     col_num = spell_err[3],
---                 })
---             end
---         end
---     end
---     return checked
--- end
-
-
--- Highlight group names
-local BAD = "TelescopeSpellCheckBad"
-local RARE = "TelescopeSpellCheckRare"
-local LOCAL = "TelescopeSpellCheckLocal"
-local CAPS = "TelescopeSpellCheckCap"
-
-local hl_map = {
-    _bad = BAD,
-    _rare = RARE,
-    _local = LOCAL,
-    _caps = CAPS,
-}
-
--- Get highlight groups
-local spell_bad = vim.api.nvim_get_hl(0, { name = "SpellBad" })
-local spell_cap = vim.api.nvim_get_hl(0, { name = "SpellCap" })
-local spell_rare = vim.api.nvim_get_hl(0, { name = "SpellRare" })
-local spell_local = vim.api.nvim_get_hl(0, { name = "SpellLocal" })
-
--- Create hightlight groups (tokyonight uses undercurls, `sp` is the color)
-vim.api.nvim_set_hl(0, BAD, { fg = spell_bad.sp })
-vim.api.nvim_set_hl(0, CAPS, { fg = spell_cap.sp })
-vim.api.nvim_set_hl(0, RARE, { fg = spell_rare.sp })
-vim.api.nvim_set_hl(0, LOCAL, { fg = spell_local.sp })
-
-
-local telescope_spell_error = function(opts)
+local telescope_spell_errors = function(opts)
     if not vim.api.nvim_get_option_value("spell", {}) then
         vim.notify("Spell checking is not enabled in the current buffer.", vim.log.levels.WARN)
         return
@@ -128,7 +91,7 @@ local telescope_spell_error = function(opts)
                 local pos = string.format("%4d:%3d", entry.line_num, entry.col_num)
                 local type_and_pos = string.format("%-5s", string.upper(entry.error_type)) .. pos
                 local highlight_len = string.len(type_and_pos)
-                local highlight_group = hl_map["_" .. entry.error_type]
+                local highlight_group = hl_groups["_" .. entry.error_type]
                 local sep = "  ▏ "
                 local dispaly_str = type_and_pos .. sep .. entry.word
                 return {
@@ -154,11 +117,12 @@ local telescope_spell_error = function(opts)
     picker:find()
 end
 
+
 return require("telescope").register_extension {
     setup = function(ext_config, config)
         -- access extension config and user config
     end,
     exports = {
-        spell_error = telescope_spell_error
+        spell_errors = telescope_spell_errors
     },
 }
